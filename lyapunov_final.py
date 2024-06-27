b3099c2189f30bdb7c74775344a3cdeac9f3f4d0
Zedonkay <ishikhar@andrew.cmu.edu>
2024-06-27 16:15:12 -0400

    kantz implemented

--- lyapunov_final.py
+++ lyapunov_final.py
@@ -96,28 +96,24 @@ def rosenstein_lyapunov(data,tau,m, min_steps, t_0, t_f,delta_t,force_minsteps):
 
 def kantz_mean_distance(vector_addresses,reconstructed_data,i):
     #calculate mean distance for kantz method
-    mean_distance = []
-    for j in range(len(vector_addresses)-i):
-        dist =0
+    distances = []
+    for j in range(len(reconstructed_data)-i):
         for k in vector_addresses[j]:
-            dist += np.log(np.linalg.norm(reconstructed_data[k+i]-reconstructed_data[j+i]))
-        if len(vector_addresses[j])>0:
-            mean_distance.append(dist/len(vector_addresses[j]))
-        else:
-            print("error")
-            print(j,i)
-    return np.mean(mean_distance)
+            if k+i<len(reconstructed_data):
+                distances.append(np.log(np.linalg.norm(reconstructed_data[k+i]-reconstructed_data[j+i])))
+    return np.mean(distances)
 def kantz_distance(vector_addresses,reconstructed_data, t_0, t_f, delta_t):
     mean_distance = []
     times = []
     for i in range(t_0,t_f):
+        if(i%5==0):
+            print(i)
         mean_distance.append(kantz_mean_distance(vector_addresses,reconstructed_data,i))
         times.append(i*delta_t)
     return times, mean_distance
 def find_epsilon_vectors(reconstructed_data,epsilon):
     vector_addresses=[]
-    for i in range(len(reconstructed_data)):
-        print(i)
+    for i in range(len(reconstructed_data)): 
         vector_locations = []
         for j in range(len(reconstructed_data)):
             if i != j:
@@ -131,10 +127,10 @@ def kantz_lyapunov(data,tau,m,t_0,t_f,delta_t,epsilon):
 
     #reconstruct data
     reconstructed_data = reconstruction(data,tau,m)
-
+    print("data reconstructed ")
     #find vectors within epsilon distance of each vector
     vector_addresses = find_epsilon_vectors(reconstructed_data,epsilon)
-
+    print("vectors found")
     #calculate log distance
     times, mean_distances = kantz_distance(vector_addresses,reconstructed_data,t_0,t_f,delta_t)
     mean_distances = np.array(mean_distances)
@@ -146,7 +142,7 @@ def kantz_lyapunov(data,tau,m,t_0,t_f,delta_t,epsilon):
 def plot_growth_factors(times, lyap_exponents,fn,control_type,frequency,test):
     """Plot Lyapunov exponents."""
     ax = plt.plot(times,lyap_exponents,label="Average divergence", color="blue")
-    plt.plot(times, fn(times),label=f"Least Squares Line", color="red")
+    plt.plot(times[80:], fn(times[80:]),label=f"Least Squares Line", color="red")
     plt.legend()
     plt.xlabel("Time")
     plt.ylabel("Average divergence")
@@ -167,9 +163,14 @@ def exponent(tau,m,min_steps, t_0,t_f,delta_t, force_minsteps, frequencies,expon
     pdata = df[['pz']]
     data=pdata.values
 
-    #calculate lyapunov exponents with rosenstein method and plot growth
-    times, data = rosenstein_lyapunov(data,tau,m,min_steps,t_0,t_f,delta_t,force_minsteps)
-    coef=np.polyfit(times,data,1)
+    # #calculate lyapunov exponents with rosenstein method 
+    # times, data = rosenstein_lyapunov(data,tau,m,min_steps,t_0,t_f,delta_t,force_minsteps)
+
+    #calculate lyapunov exponents with kantz method
+    times, data = kantz_lyapunov(data,tau,m,t_0,t_f,delta_t,0.1)
+    
+    #plot growth
+    coef=np.polyfit(times[80:],data[80:],1)
     poly1d_fn = np.poly1d(coef)
     plot_growth_factors(times, data,poly1d_fn,control_type,frequency,test)
 
@@ -188,8 +189,8 @@ def main():
     delta_t = 0.01
     min_steps = 100
     force_minsteps = False
-    t_0 =80
-    t_f =150
+    t_0 =0
+    t_f =200
 
 
     #track centralised exponents
@@ -201,26 +202,30 @@ def main():
     distributed_frequencies = []
 
     #calculate exponents for centralised control
-    print("Calculating exponents for centralised control")
-    centralised = [[100,'1'],[100,'2'], [100,'3'],[140,'1'],[140,'2'],
-                   [140,'3'], [140,'4'],[180,'1'],[220,'1'],[220,'2'],
-                   [220,'3'],[220,'4'],[260,'1'],[260,'2'],[260,'3'],
-                   [260,'4'],[300,'1'],[320,'1'],[320,'2'],[320,'3'],
-                   [330,'1'],[350,'1'],[350,'2'],[350,'3'],[350,'4'],
+
+    centralised = [[100,'1'],[100,'2'],[140,'1'],[180,'1'],
+                   [180,'2'],[180,'3'],[220,'1'],[220,'2'],
+                   [220,'3'],[260,'1'],[260,'2'],[260,'3'],
+                   [260,'4'],[300,'1'],[320,'1'],[320,'2'],
+                   [330,'1'],[350,'1'],[350,'2'],[350,'3'],
                    [370,'1'],[370,'2'],[370,'3']]
+   
+    distributed =[[100,'1'],[100,'2'],[100,'3'],[140,'1'],[140,'2'],
+                  [180,'1'],[180,'2'],[220,'1'],[220,'2'],
+                  [260,'1'],[260,'2'],[260,'3'],[300,'1'],[300,'2'],[300,'3'],
+                  [350,'1'],[350,'2'],[370,'1'],[370,'2'],[400,'1'],
+                  [400,'2'],[500,'1'],[600,'1'],[700,'1'],[800,'1'],
+                  [1000,'1'],[1040,'1'],[1120,'1'],[1160,'1'],[1200,'1'],
+                  [1440,'1'],[1600,'1'], [1800,'1'],[2000,'1']]
+    
+
+    print("Calculating exponents for centralised control")
     for i in centralised:
         exponent(tau,m,min_steps,t_0,t_f,delta_t,force_minsteps,centralised_frequencies,centralised_exponents,i[0],'centralised',i[1])
         print(f"Exponent for centralised {i[0]}Hz test {i[1]} calculated")
     
     #calculate exponents for distributed control
     print("Calculating exponents for distributed control")
-    distributed =[[100,'1'],[100,'2'],[100,'3'],[140,'1'],[140,'2'],[140,'3'],
-                  [180,'1'],[180,'2'],[180,'3'],[220,'1'],[220,'2'],[220,'3'],
-                  [260,'1'],[260,'2'],[260,'3'],[300,'1'],[300,'2'],[300,'3'],
-                  [350,'1'],[350,'2'],[350,'3'],[370,'1'],[370,'2'],[370,'3'],[400,'1'],
-                  [400,'2'],[400,'3'],[500,'1'],[600,'1'],[700,'1'],[800,'1'],
-                  [1000,'1'],[1040,'1'],[1080,'1'],[1120,'1'],[1160,'1'],[1200,'1'],
-                  [1400,'1'],[1440,'1'],[1480,'1'],[1600,'1'], [1800,'1'],[2000,'1']]
     for i in distributed:
         exponent(tau,m,min_steps,t_0,t_f,delta_t,force_minsteps,distributed_frequencies,distributed_exponents,i[0],'distributed',i[1])
         print(f"Exponent for distributed {i[0]}Hz test {i[1]} calculated")
@@ -229,9 +234,9 @@ def main():
     plot_exponents(centralised_frequencies,centralised_exponents,distributed_frequencies,distributed_exponents)
 
     #store exponents and frequencies in csv
-    data = pd.DataFrame(centralised_exponents,index=centralised_frequencies,columns=['lyapunov_exponent'])
+    data = pd.DataFrame(centralised_exponents,index=centralised_frequencies,columns=['mean divergence'])
     data.to_csv("6_Results/clean_data/centralised/centralised_exponents.csv",index=True)
-    data = pd.DataFrame(distributed_exponents,index=distributed_frequencies,columns=['lyapunov_exponent'])
+    data = pd.DataFrame(distributed_exponents,index=distributed_frequencies,columns=['mean divergence'])
     data.to_csv("6_Results/clean_data/distributed/distributed_exponents.csv",index=True)
     
 if __name__ == "__main__":
