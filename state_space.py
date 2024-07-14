29f535591dafac8f2403e61f8c0bd80125cfc770
Zedonkay <144646939+Zedonkay@users.noreply.github.com>
2024-07-13 23:17:12 -0400

    attempted per cycle adjustment

--- state_space.py
+++ state_space.py
@@ -55,38 +55,77 @@ def find_lines(pos_x,tolerance):
     p_1 = (lines[0][0]+lines[0][1])/2
     p_2 = (lines[len(lines)-1][0]+lines[len(lines)-1][1])/2
     return int(round(p_1)),int(round(p_2))
-def find_peaks(pos_z):
-    peaks = [0]
+def find_top_peaks(pos_z):
+    peaks = []
     for i in range(1,len(pos_z)-1):
-        if(pos_z[i]<0.172):
-            continue
         if pos_z[i]>pos_z[i-1] and pos_z[i]>pos_z[i+1]:
-            peaks.append(i)
+            if(len(pos_z)-i>50):
+                next_positions = pos_z[i:i+50]
+            else:
+                next_positions = pos_z[i:]
+            if(i>50):
+                previous_positions = pos_z[i-50:i]
+            else:
+                previous_positions = pos_z[:i]
+            if len(next_positions[next_positions>pos_z[i]])==0 and len(previous_positions[previous_positions>pos_z[i]])==0:
+                peaks.append(i)
     peaks.append(len(pos_z)-1)
-    return peaks
+    return peaks  
+def find_bottom_peaks(pos_z):
+    peaks = []
+    for i in range(1,len(pos_z)-1):
+        if pos_z[i]<pos_z[i-1] and pos_z[i]<pos_z[i+1]:
+            if(len(pos_z)-i>50):
+                next_positions = pos_z[i:i+50]
+            else:
+                next_positions = pos_z[i:]
+            if(i>50):
+                previous_positions = pos_z[i-50:i]
+            else:
+                previous_positions = pos_z[:i]
+            if len(next_positions[next_positions<pos_z[i]])==0 and len(previous_positions[previous_positions<pos_z[i]])==0:
+                peaks.append(i)
     
-def compute_plotting_points(df,peaks,min,max):
-    pos_x,pos_y,pos_z = df['px'].values,df['py'].values,df['pz'].values
-    positions = df[['px','py','pz']].values
-    range_x = pos_x[max]-pos_x[min]
-    range_y = pos_y[max]-pos_y[min]
-    W=np.array([range_x,range_y,0])
-    W = W/np.linalg.norm(W)
-    V = np.cross(W,np.array([1,0,0]))
-    V = V/np.linalg.norm(V)
-    U = np.cross(V,W)
-    U = U/np.linalg.norm(U)
-    matrix = np.array([U,V,W])
-    plotting = []
-    for position in positions:
-        plotting.append(np.dot(matrix,position))
-    plotting = np.array(plotting)
-    return plotting[:,0],plotting[:,1],plotting[:,2]
+    return peaks
+def compute_midpoints(bottom_peaks,top_peaks,pos_z):
+    midpoints = []
+    for i in range(min(len(top_peaks)-1,len(bottom_peaks)-1)):
+        midpoints.append(int((top_peaks[i]+bottom_peaks[i])/2))
+    midpoints.append(len(pos_z)-1)
+    midpoints = np.array(midpoints)
+    np.insert(midpoints,0,0)
+    return midpoints
 
 
+def rotate_data(data,min,max,initial_x,initial_z):
+    vector = [data[max][0]-data[min][0],
+              data[max][1]-data[min][1],
+              data[max][2]-data[min][2]]
+    angle = -np.arctan(vector[0]/vector[1])
+    rotated_data = []
+    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle), 0],
+                                [np.sin(angle), np.cos(angle), 0],
+                                [0, 0, 1]])
+    for i in range(min,max):
+        current_data= np.dot(rotation_matrix,np.array([data[i][0],data[i][1],data[i][2]]))
+        current_data[0] = current_data[0]-initial_x+data[min][0]
+        current_data[2] = current_data[2]-initial_z+data[min][2]
+        rotated_data.append(current_data)
+    rotated_data = np.array(rotated_data)
+    return rotated_data    
+def compute_plotting_points(data,bottom_peaks):
+    initial_x = data[bottom_peaks[0]][0]
+    initial_z= data[bottom_peaks[0]][2]
+    for i in range(len(bottom_peaks)-1):
+        if i == 0:
+            rotated_data = rotate_data(data,bottom_peaks[i],bottom_peaks[i+1],initial_x,initial_z)
+        else:
+            rotated_data = np.concatenate((rotated_data,rotate_data(data,bottom_peaks[i],bottom_peaks[i+1],initial_x,initial_z)))
+    rotated_data = np.array(rotated_data)
+    return rotated_data[:,0],rotated_data[:,1],rotated_data[:,2]
 
 def plot_2d(timestamps, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z,acc_x,acc_y,acc_z, 
-            roll, pitch, yaw,frequency,test,control_type,min,max):
+            roll, pitch, yaw,frequency,test,control_type,midpoints):
     # Plot individual 2D plots for pos_x, pos_y, pos_z, roll, pitch, yaw
     fig, axs = plt.subplots(4, 3, figsize=(18, 10))
     if control_type == "centralised":
@@ -96,8 +135,6 @@ def plot_2d(timestamps, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z,acc_x,acc_y,acc
 
 
     axs[0, 0].plot(timestamps, pos_x)
-    axs[0,0].scatter(timestamps[min],pos_x[min],color='red')
-    axs[0,0].scatter(timestamps[max],pos_x[max],color='red')
     axs[0, 0].set_xlabel('Timestamp')
     axs[0, 0].set_ylabel('Position X')
     axs[0, 0].set_title('Position X vs. Timestamp')
@@ -184,7 +221,7 @@ def plot_3d_phase_space_vel(vel_x, vel_y, vel_z,frequency,test, control_type):
     plt.clf()
     plt.close
 
-def plot_3d_phase_space_pos(pos_x, pos_y, pos_z,frequency, test,control_type):
+def plot_3d_phase_space_pos(pos_x, pos_y, pos_z,frequency, test,control_type,bottom_peaks,top_peaks):
     # Plot 3D phase space plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -199,6 +236,76 @@ def plot_3d_phase_space_pos(pos_x, pos_y, pos_z,frequency, test,control_type):
     plt.savefig(fg.store_clean_data(frequency,test,control_type)+"3d_state_space.png")
     plt.clf()
     plt.close()
+def plot_rotated_helix(pos_x, pos_y, pos_z,frequency,test,control_type,bottom_peaks):
+    # Plot 3D phase space plot
+    bottom_peaks = np.array(bottom_peaks)
+    bottom_peaks = bottom_peaks[bottom_peaks<min(len(pos_x),len(pos_y),len(pos_z))]
+
+    fig, axs = plt.subplots(2, 2, figsize=(18, 10))
+    fig.suptitle('Rotated Helix Plot')
+
+    axs[0, 0].plot(pos_x, pos_y)
+    axs[0, 0].scatter(pos_x[bottom_peaks],pos_y[bottom_peaks])
+    axs[0, 0].set_xlabel('Position X')
+    axs[0, 0].set_ylabel('Position Y')
+    axs[0, 0].set_title('Position X vs. Position Y')
+
+    axs[0, 1].plot(pos_x, pos_z)
+    axs[0,1].scatter(pos_x[bottom_peaks],pos_z[bottom_peaks])
+    axs[0, 1].set_xlabel('Position X')
+    axs[0, 1].set_ylabel('Position Z')
+    axs[0, 1].set_title('Position X vs. Position Z')
+
+    axs[1, 0].plot(pos_y, pos_z)
+    axs[1,0].scatter(pos_y[bottom_peaks],pos_z[bottom_peaks])
+    axs[1, 0].set_xlabel('Position Y')
+    axs[1, 0].set_ylabel('Position Z')
+    axs[1, 0].set_title('Position Y vs. Position Z')
+
+    axs[1,1]=plt.subplot(224,projection='3d')
+    axs[1,1].scatter(pos_x[bottom_peaks],pos_y[bottom_peaks],pos_z[bottom_peaks])
+    axs[1,1].plot(pos_x,pos_y,pos_z)
+    axs[1,1].set_xlabel('Position X')
+    axs[1,1].set_ylabel('Position Y')
+    axs[1,1].set_zlabel('Position Z')
+
+    plt.tight_layout()
+    plt.savefig(fg.store_clean_data(frequency,test,control_type)+"rotated_helix.png")
+
+def plot_original_helix(pos_x, pos_y, pos_z,frequency,test,control_type,bottom_peaks):
+    # Plot 3D phase space plot
+    fig, axs = plt.subplots(2, 2, figsize=(18, 10))
+    fig.suptitle('Original Helix Plot')
+
+    axs[0, 0].plot(pos_x, pos_y)
+    axs[0, 0].scatter(pos_x[bottom_peaks],pos_y[bottom_peaks])
+    axs[0, 0].set_xlabel('Position X')
+    axs[0, 0].set_ylabel('Position Y')
+    axs[0, 0].set_title('Position X vs. Position Y')
+
+    axs[0, 1].plot(pos_x, pos_z)
+    axs[0,1].scatter(pos_x[bottom_peaks],pos_z[bottom_peaks])
+    axs[0, 1].set_xlabel('Position X')
+    axs[0, 1].set_ylabel('Position Z')
+    axs[0, 1].set_title('Position X vs. Position Z')
+
+    axs[1, 0].plot(pos_y, pos_z)
+    axs[1,0].scatter(pos_y[bottom_peaks],pos_z[bottom_peaks])
+    axs[1, 0].set_xlabel('Position Y')
+    axs[1, 0].set_ylabel('Position Z')
+    axs[1, 0].set_title('Position Y vs. Position Z')
+
+    axs[1,1]=plt.subplot(224,projection='3d')
+    axs[1,1].scatter(pos_x[bottom_peaks],pos_y[bottom_peaks],pos_z[bottom_peaks])
+    axs[1,1].plot(pos_x,pos_y,pos_z)
+    axs[1,1].set_xlabel('Position X')
+    axs[1,1].set_ylabel('Position Y')
+    axs[1,1].set_zlabel('Position Z')
+
+    plt.tight_layout()
+    plt.savefig(fg.store_clean_data(frequency,test,control_type)+"original_helix.png")
+
+
 
 def plot_3d_euler_state_space(timestamps, roll, pitch, yaw,frequency,test,control_type):
     # Plot 3D Euler state space plot
@@ -216,12 +323,14 @@ def plot_3d_euler_state_space(timestamps, roll, pitch, yaw,frequency,test,contro
     plt.clf()
     plt.close()
 
-def plot_gait(plot_x,plot_y,plot_z,peaks,frequency,test,control_type):
+def plot_gait(plot_x,plot_y,plot_z,bottom_peaks,frequency,test,control_type):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    color = cm.rainbow(np.linspace(0,1,len(peaks)))
-    for i in range(len(peaks)-1):
-        ax.plot(plot_x[peaks[i]:peaks[i+1]],plot_y[peaks[i]:peaks[i+1]],color=color[i])
+
+    for i in range(len(bottom_peaks)-1):
+        color = cm.rainbow(np.linspace(0,1,bottom_peaks[i+1]-bottom_peaks[i]))
+        for j in range(bottom_peaks[i],bottom_peaks[i+1]-1,4):
+            ax.plot(plot_x[j:j+5],plot_y[j:j+5],color=color[j-bottom_peaks[i]])
     ax.set_xlabel('Transverse Plane')
     ax.set_ylabel('Frontal Plane')
     if control_type == "centralised":
@@ -231,23 +340,8 @@ def plot_gait(plot_x,plot_y,plot_z,peaks,frequency,test,control_type):
     plt.savefig(fg.store_clean_data(frequency,test,control_type)+"gait.png")
     plt.clf()
     plt.close()
-def plot_plotting_x(frequency,test,control_type,plot_x,plot_z):
-    plt.plot(plot_x,plot_z)
-    plt.xlabel('Plotting X')
-    plt.ylabel('Plotting Z')
-    plt.title('Plotting X vs Plotting Z')
-    plt.savefig(fg.store_clean_data(frequency,test,control_type)+'x_vs_z.png')
-    plt.clf()
-    plt.close()
-def plot_plotting_y(frequency,test,control_type,plot_y,plot_z):
-    plt.plot(plot_y,plot_z)
-    plt.xlabel('Plotting Y')
-    plt.ylabel('Plotting Z')
-    plt.title('Plotting Y vs Plotting Z')
-    plt.savefig(fg.store_clean_data(frequency,test,control_type)+'y_vs_z.png')
-    plt.clf()
-    plt.close()
-def main(frequency,control_type,test,min,max):
+
+def main(frequency,control_type,test):
     # Generate file path
     file_path = fg.filename_clean(frequency,test,control_type)
 
@@ -257,36 +351,34 @@ def main(frequency,control_type,test,min,max):
     # Convert quaternion to roll, pitch, yaw angles
     roll, pitch, yaw = quaternion_to_euler(quaternion)
     
-   #check that max value is in range
-    if max>len(pos_x):
-        max = 1050
-    if max>len(pos_x):
-        max = 770
+   
 
     # find peaks
-    peaks = find_peaks(pos_z)
+    top_peaks = find_top_peaks(pos_z)
+    bottom_peaks = find_bottom_peaks(pos_z)
+    midpoints = compute_midpoints(bottom_peaks,top_peaks,pos_z)
 
     #compute plotting points
-    plot_x,plot_y,plot_z = compute_plotting_points(df,peaks,min,max)
+    plot_x,plot_y,plot_z = compute_plotting_points(np.column_stack((pos_x,pos_y,pos_z)),bottom_peaks)
 
     
 
     # Plot 2D positions and angles
     plot_2d(timestamps, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, 
-            acc_x,acc_y,acc_z,roll, pitch, yaw, frequency,test,control_type,min,max)
+            acc_x,acc_y,acc_z,roll, pitch, yaw, frequency,test,control_type,midpoints)
 
     # Plot 3D phase space
-    plot_3d_phase_space_pos(pos_x, pos_y, pos_z,frequency,test,control_type)
+    plot_3d_phase_space_pos(pos_x, pos_y, pos_z,frequency,test,control_type,bottom_peaks,top_peaks)
     plot_3d_phase_space_vel(vel_x, vel_y, vel_z,frequency,test,control_type)
 
     # Plot 3D Euler state space
     plot_3d_euler_state_space(timestamps, roll, pitch, yaw,frequency,test,control_type)
     
     #plot 2d gait cycle
-    plot_gait(plot_x,plot_y,plot_z,peaks, frequency,test,control_type)
+    plot_gait(plot_x,plot_y,plot_z,bottom_peaks, frequency,test,control_type)
 
-    #plot plotting x vs z
-    plot_plotting_x(frequency,test,control_type,plot_x,plot_z)
+    #plot rotated data
+    plot_rotated_helix(plot_x,plot_y,plot_z,frequency,test,control_type,bottom_peaks)
 
-    #plot plotting y vs z
-    plot_plotting_y(frequency,test,control_type,plot_y,plot_z)
+    #plot original data
+    plot_original_helix(pos_x, pos_y, pos_z,frequency,test,control_type,bottom_peaks)
